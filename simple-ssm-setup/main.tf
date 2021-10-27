a88f7e7cd8ff411fa43ca02742a165c0e1339b65
Ed Randall <github@edrandall.uk>
2021-10-27 14:10:33 +0000

    Corrected env_prefix in variables.tf

--- simple-ssm-setup/main.tf
+++ simple-ssm-setup/main.tf
@@ -2,14 +2,14 @@ terraform {
   required_version = ">= 0.12"
 
   backend "s3" {
-    bucket = "edrandall-tf-backend"
-    key    = "tf-backends/simple-ssm-setup"
-    region = "eu-north-1"
+    bucket  = "edrandall-tf-backend"
+    key     = "tf-backends/simple-ssm-setup"
+    region  = "eu-north-1"
     profile = "rackspace"
   }
 }
 
 provider "aws" {
-  region = var.region
+  region  = var.region
   profile = "rackspace"
 }
